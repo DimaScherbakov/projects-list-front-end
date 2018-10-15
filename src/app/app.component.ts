ab5cd2be75f1aa894d4c67cc103f1c5fd23c946c
dimaScherbakov <dima.r.scherbakov@gmail.com>
2018-10-15 13:59:35 +0300

    fix everything

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projects-list-front-end';
-<<<<<<< HEAD
-
-  constructor(){ }
-
-=======
 needProjects:boolean;
   constructor(){ }
     onChanged(d:any){
         this.needProjects=d;
     }
->>>>>>> aec24e330f0b504ac87fd2191377834a55360553
 }
